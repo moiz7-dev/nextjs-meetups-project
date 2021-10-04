3fc072e962059fb44ea1bc19f7338fbfa204e463
moiz <moizratlamwala78652@gmail.com>
2021-10-05 02:08:12 +0530

    fallback fixed

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -35,7 +35,7 @@ export async function getStaticPaths() {
   const meetupIds = await meetupsCollection.find({}, { _id: 1 }).toArray();
 
   return {
-    fallback: 'blocking',
+    fallback: true,
     paths: meetupIds.map((m) => ({ params: { meetupId: m._id.toString() } })),
   };
 }
